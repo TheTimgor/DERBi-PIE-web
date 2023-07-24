6dd76abc8c201493f2d4e1a853973ca0cb6e7600
garyantonyo <garyantonyo@gmail.com>
2023-07-24 17:46:02 -0400

    checkpoint before i mess up the psuedoRegex translation

--- routes/results.js
+++ routes/results.js
@@ -6,13 +6,23 @@ const uri = 'mongodb://localhost:27017';
 const dbName = 'DERBI-PIE';
 const client = new MongoClient(uri);
 
+// region helpers
+function mapRegexes(field, regexes){
+    return {$and: regexes.map((el) => ({[field]: {$regex: el}}))}
+}
+
+function escapeRegExp(str) {
+    return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
+// endregion
+
 router.get('/', async (req, res) => {
     // Perform the database query to retrieve search results
-    let searchResults
+    let searchResults = []
+    await getIDs(req.query);
     // if (req.query.search || req.query.search == ""){
-        searchResults = await performSearch(req.query.search);
+    //     searchResults = await performSearch(req.query.search);
     // }else{
-    //     searchResults = await getIDs(req.query);
     // }
     // if there is a single result, we redirect to it
     if(searchResults.length === 1){
@@ -23,9 +33,48 @@ router.get('/', async (req, res) => {
     // Render the search results page
     res.render('results', { results: searchResults });
 });
+const charReplacements = {
+    // Add more character replacements as needed
+};
 
-function mapRegexes(field, regexes){
-    return {$and: regexes.map((el) => ({[field]: {$regex: el}}))}
+const linguisticFeatures = {
+    stop: 'pʰ|bʰ|tʰ|dʰ|ḱʰ|ǵʰ|kʰ|gʰ|kʷʰ|T|K|k\'ʰ|ǵʰ|ǵ\'|kʷ|gʷʰ|g\'',
+    fricative: 'regex_fricative',
+    glide: 'regex_glide',
+    T: 'regex_T',
+    K: 'regex_K',
+    H: 'regex_H',
+    // Add more linguistic features as needed
+};
+
+function generateRegex(pseudoRegex) {
+    // remove any accidental repeated dashes, it messes up subsequent things
+    pseudoRegex = pseudoRegex.replace(/-+/, "-")
+
+    // Replace custom characters with corresponding regex patterns
+    for (const char in charReplacements) {
+        pseudoRegex = pseudoRegex.replace(new RegExp(char, 'g'), charReplacements[char]);
+    }
+
+    // Replace linguistic features with corresponding regex patterns
+    for (const feature in linguisticFeatures) {
+        // there are three cases
+        // 1. if there is a - followed by a negative group
+        pseudoRegex = pseudoRegex.replace(new RegExp(`(-\\[-${feature}\\])`, 'g'), `(?![^\\d]+(?:${linguisticFeatures[feature]}))`)
+        // 2. if there is a negative group without the preceding -
+        pseudoRegex = pseudoRegex.replace(new RegExp(`(\\[-${feature}\\])`, 'g'), `(?!${linguisticFeatures[feature]})`)
+        // 3. if there is a positive group
+        pseudoRegex = pseudoRegex.replace(new RegExp(`(\\[\\+${feature}\\])`, 'g'), `(?:${linguisticFeatures[feature]})`)
+
+    }
+
+    // Replace "#" with word boundary \b
+    pseudoRegex = pseudoRegex.replace(/#/g, '\\b');
+
+    // Replace "-" with 1 or more non whitespace regex
+    pseudoRegex = pseudoRegex.replace(/-/g, '[^\\s]+');
+
+    return pseudoRegex;
 }
 
 async function getIDs(data) {
@@ -34,7 +83,7 @@ async function getIDs(data) {
 
     // todo: I have no idea what word shape refers to
     let orQuery = []
-    let regexes = shapedRegexes(data["rootBegin"], data["rootContains"], data["rootShape"])
+    let regexes = generateRegex(data["rootShape"])
     console.log(regexes)
 
     if(data.checkRoots){
@@ -61,10 +110,6 @@ async function getIDs(data) {
         // orQuery.push({meaning: shapedQuery})
     }
     return []
-}
-
-function escapeRegExp(str) {
-    return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 }
 
 function shapedRegexes(begins, contains, shape) {
